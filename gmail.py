c1b28cfa2bf0dcc7e78fba86e456552e4440b363
SL34K <30938710+SL34K@users.noreply.github.com>
2018-03-12 23:36:05 +0000

    Update gmail.py
    added headers

--- gmail.py
+++ gmail.py
@@ -22,6 +22,12 @@ def main():
     email = prefix+'+'+username+'@gmail.com'
     print(email)
     sesh = requests.session()
+    sesh.headers = {
+        'Origin':'https://epicgames.com',
+        'Referer':'https://epicgames.com',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
+    }
+    sesh.headers.update()
     formget = sesh.get(signuppage)
     soup = BeautifulSoup(formget.content,'html.parser')
     token = soup.find('input', {'name': 'X-XSRF-TOKEN'}).get('value')
